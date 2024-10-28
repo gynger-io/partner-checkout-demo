a90db47e8919d5f5628ec783f476d2ededba9243
hogggy <will@mxv.vc>
2024-10-27 18:47:30 -0600

    use staging url

--- src/server/gynger-client.ts
+++ src/server/gynger-client.ts
@@ -6,16 +6,16 @@ const client = axios.create({
 });
 
 export async function listWebhooks() {
-  const result = await client.get('https://sandbox.api.gynger.io/v1/webhooks');
+  const result = await client.get('https://staging.api.gynger.io/v1/webhooks');
   return result.data;
 }
 
 export function deleteWebhooks(webhookId: string) {
-  return client.delete(`https://sandbox.api.gynger.io/v1/webhooks/${webhookId}`);
+  return client.delete(`https://staging.api.gynger.io/v1/webhooks/${webhookId}`);
 }
 
 export async function createWebhook() {
-  const result = await client.post('https://sandbox.api.gynger.io/v1/webhooks', {
+  const result = await client.post('https://staging.api.gynger.io/v1/webhooks', {
     url: `${process.env.NGROK_URL}/webhooks`,
     events: ['account.status.updated', 'offer.status.updated', 'checkout.session.status.updated'],
   });
@@ -23,19 +23,19 @@ export async function createWebhook() {
 }
 
 export async function createCheckoutSession(data: { amountDollars: number; productDescription: string }) {
-  const result = await client.post('https://sandbox.api.gynger.io/v1/checkout/sessions', data);
+  const result = await client.post('https://staging.api.gynger.io/v1/checkout/sessions', data);
   return result.data;
 }
 
 export async function updateSessionStatus(sessionId: string, status: string) {
-  const result = await client.put(`https://sandbox.api.gynger.io/v1/sandbox/checkout/sessions/${sessionId}/status`, {
+  const result = await client.put(`https://staging.api.gynger.io/v1/sandbox/checkout/sessions/${sessionId}/status`, {
     status,
   });
   return result.data;
 }
 
 export async function updateOfferStatus(offerId: string, status: string) {
-  const result = await client.put(`https://sandbox.api.gynger.io/v1/sandbox/offers/${offerId}/status`, {
+  const result = await client.put(`https://staging.api.gynger.io/v1/sandbox/offers/${offerId}/status`, {
     status,
   });
   return result.data;
