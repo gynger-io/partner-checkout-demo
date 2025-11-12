4bbd32e82b40d3352835484d85afd80544b9afc9
Moria Or <moria@gynger.io>
2025-11-12 22:44:53 +0200

    add preview toggle

--- src/client/components/CheckoutButton.tsx
+++ src/client/components/CheckoutButton.tsx
@@ -10,7 +10,7 @@ declare global {
   }
 }
 
-export default function CheckoutButton(props: { deal: Deal; settings: any }) {
+export default function CheckoutButton(props: { deal: Deal; settings: any; previewEnabled: boolean }) {
   useEffect(() => {
     const myNode = document.getElementById(`gynger-checkout-widget-${props.deal.checkoutId}`);
     myNode.innerHTML = '';
@@ -18,13 +18,13 @@ export default function CheckoutButton(props: { deal: Deal; settings: any }) {
       token: props.deal.checkoutId, // Replace with actual token returned from createCheckoutSession request
       containerId: `gynger-checkout-widget-${props.deal.checkoutId}`,
       publicKey: window.APP_CONFIG.GYNGER_PUBLIC_KEY,
-      previewEnabled: true,
+      previewEnabled: props.previewEnabled,
       onError: (error: Error) => {
         console.error(error.message);
         alert(`Error: ${error.message}`);
       },
       styles: props.settings,
     });
-  }, [props.settings]);
+  }, [props.settings, props.previewEnabled]);
   return <div id={`gynger-checkout-widget-${props.deal.checkoutId}`} />;
 }
